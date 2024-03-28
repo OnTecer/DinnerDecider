902922b3f30987137b36ad3a7a8989b5003b7137
School <141075183+OnTecer@users.noreply.github.com>
2024-03-28 09:38:21 -0500

    added recipe_is_valid method to DatabaseAccesser

--- database_access.py
+++ database_access.py
@@ -15,6 +15,50 @@ class DatabaseAccesser(UserDict):
             new_raw_data = json.dumps(self.data, indent=4)
             outfile.write(new_raw_data)
 
+    # These are required:
+    # "name": str, "ingredients": list[str], "minutes_to_make": int, "date_last_eaten": date
+    # These are only required if they are turned on in settings, and are on a scale of 0 to 20
+    # "food_rating": int, "health_rating": int
+    def recipe_is_valid(self, recipe: dict) -> bool:
+        if "name" not in recipe:
+            return False
+        elif recipe["name"] is not str:
+            return False
+
+        elif "ingredients" not in recipe:
+            return False
+        elif recipe["ingredients"] is not list[str]:
+            return False
+
+        elif "minutes_to_make" not in recipe:
+            return False
+        elif recipe["minutes_to_make"] is not int:
+            return False
+
+        elif "date_last_eaten" not in recipe:
+            return False
+        elif recipe["date_last_eaten"] is not date:
+            return False
+
+        elif self["settings"]["food_rating_is_stored"] == True:
+            if "food_rating" not in recipe:
+                return False
+            elif recipe["food_rating"] is not int:
+                return False
+            elif not 0 <= recipe["food_rating"] <= 20:
+                return False
+
+        elif self["settings"]["health_rating_is_stored"] == True:
+            if "health_rating" not in recipe:
+                return False
+            elif recipe["health_rating"] is not int:
+                return False
+            elif not 0 <= recipe["health_rating"] <= 20:
+                return False
+
+        else:
+            return True
+
     def add_recipe(self, recipe: dict) -> None:
         self.data["recipes"].append(recipe)
 
