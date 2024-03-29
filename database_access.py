3b41e36c2d8b788b8831e2d2553364513332627c
School <141075183+OnTecer@users.noreply.github.com>
2024-03-28 23:33:09 -0500

    started work on a way to print out the/a recipe(s)

--- database_access.py
+++ database_access.py
@@ -40,7 +40,7 @@ class DatabaseAccesser(UserDict):
         elif recipe["date_last_eaten"] is not date:
             return False
 
-        elif self["settings"]["food_rating_is_stored"] == True:
+        elif self.data["settings"]["food_rating_is_stored"]:
             if "food_rating" not in recipe:
                 return False
             elif recipe["food_rating"] is not int:
@@ -48,7 +48,7 @@ class DatabaseAccesser(UserDict):
             elif not 0 <= recipe["food_rating"] <= 20:
                 return False
 
-        elif self["settings"]["health_rating_is_stored"] == True:
+        elif self.data["settings"]["health_rating_is_stored"]:
             if "health_rating" not in recipe:
                 return False
             elif recipe["health_rating"] is not int:
@@ -66,8 +66,54 @@ class DatabaseAccesser(UserDict):
         for recipe_it in range(len(self.data["recipes"])):
             if self.data["recipes"][recipe_it]["name"] == recipe_name:
                 del self.data["recipes"][recipe_it]
+                return None
+
+    @staticmethod
+    def __remove_time_of_day_from_date(self, raw_date: str) -> str:
+        comma_index: int = raw_date.index(",")
+        deconstucted_raw_date: list[str] = list(raw_date)
+        for char in range(comma_index, len(raw_date)):
+            del deconstucted_raw_date[char]
+
+        final_date: str = ""
+        for char in deconstucted_raw_date:
+            final_date += char
+
+        return final_date
+
+    @staticmethod
+    def __list_ingredients(ingredients: list[str]) -> str:
+        ingredient_list: str = ""
+        for ingredient in range(len(ingredients) - 2):
+            ingredient_list += ingredients[ingredient] + ", "
+        ingredient_list += ingredients[len(ingredients) - 1]
+
+        return ingredient_list
+
+    def print_recipe(self, recipe: dict[str]) -> None:
+        name: str = self.data["recipes"][recipe]["name"]
+        print(name + ":")
+
+        ingredients_raw: list[str] = self.data["recipes"][recipe]["ingredients"]
+        ingredients_listed_out: str = self.__list_ingredients(ingredients_raw)
+        print("\t" + "ingredients: " + ingredients_listed_out)
+
+        minutes_to_make: str = str(self.data["recipes"]["minutes_to_make"])
+        print("\t" + "time to make: " + minutes_to_make + " minutes")
+
+        todays_date: date = date.today()
+
+        print("\t" + "days since last eaten: " + str(self.data["recipes"][""]))
+
+    def print_all_recipes(self) -> None:
+        for recipe in self.data["recipes"]:
+            self.print_recipe(recipe)
 
 
 # Code for testing
 if __name__ == "__main__":
-    pass
+    DB: DatabaseAccesser = DatabaseAccesser()
+    todayss_date = date.today()
+    print(todayss_date, type(todayss_date))
+    print(abs(todayss_date - date(2023, 2, 27)))
+    print(type(abs(todayss_date - date(2023, 2, 27))))
