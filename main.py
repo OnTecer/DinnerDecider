d895da7362403b6bf2f3e9c45c3bedaf3107bf37
School <141075183+OnTecer@users.noreply.github.com>
2024-04-10 13:36:14 -0500

    Started user interaction portion

--- main.py
+++ main.py
@@ -1,8 +1,38 @@
-from json import *
+from database_access import *
 
 
 def main() -> None:
-    print("Hello World!")
+    while True:
+        command: str = ask_for_command()
+
+
+def ask_for_command() -> str:
+    raw_command: str = input("type in your command:  ")
+    final_command: str = ""
+    for char in range(len(raw_command)):
+        if raw_command[char] == " ":
+            pass
+        else:
+            final_command += raw_command[char]
+
+    return final_command
+
+
+def intro() -> None:
+    print(
+"""
+type in any of the following commands:
+
+decide (decide what you should eat today)
+add (adds a new recipe to the list of recipes)
+delete (deletes a recipe from the list of recipes)
+find (finds a recipe from the list of recipes)
+change (changes a recipe)
+eat (changes the days since last eaten for a recipe to 0)
+show (shows settings)
+modify (changes the settings)
+"""
+)
 
 
 if __name__ == "__main__":
