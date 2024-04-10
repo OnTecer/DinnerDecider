dc136fe3a72ce825669dc1d669af1cdb6b0c28fd
School <141075183+OnTecer@users.noreply.github.com>
2024-04-10 14:09:09 -0500

    Added the user commands

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from database_access import *
 
 
+##############################################################################
+# Main
+##############################################################################
 def main() -> None:
+    intro()
     while True:
-        command: str = ask_for_command()
+        ask_for_command()
 
 
+##############################################################################
+# Processes
+##############################################################################
 def ask_for_command() -> str:
     raw_command: str = input("type in your command:  ")
     final_command: str = ""
@@ -15,25 +22,75 @@ def ask_for_command() -> str:
         else:
             final_command += raw_command[char]
 
+    try:
+        eval(f"command_{final_command}()")
+    except SyntaxError:
+        print(f"{final_command} is not a command")
+
     return final_command
 
 
 def intro() -> None:
     print(
-"""
-type in any of the following commands:
+        """
+        type in any of the following commands:
+        
+        decide (decide what you should eat today)
+        add (adds a new recipe to the list of recipes)
+        delete (deletes a recipe from the list of recipes)
+        find (finds a recipe from the list of recipes)
+        change (changes a recipe)
+        eat (changes the days since last eaten for a recipe to 0)
+        show (shows settings)
+        modify (changes the settings)
+        quit (quits the program)
+        """
+    )
 
-decide (decide what you should eat today)
-add (adds a new recipe to the list of recipes)
-delete (deletes a recipe from the list of recipes)
-find (finds a recipe from the list of recipes)
-change (changes a recipe)
-eat (changes the days since last eaten for a recipe to 0)
-show (shows settings)
-modify (changes the settings)
-"""
-)
+
+##############################################################################
+# Commands
+##############################################################################
+def command_decide() -> None:
+    pass
 
 
+def command_add() -> None:
+    pass
+
+
+def command_delete() -> None:
+    pass
+
+
+def command_find() -> None:
+    pass
+
+
+def command_change() -> None:
+    pass
+
+
+def command_eat() -> None:
+    pass
+
+
+def command_show() -> None:
+    pass
+
+
+def command_modify() -> None:
+    pass
+
+
+def command_quit() -> None:
+    print("Quitting program")
+    quit()
+
+
+##############################################################################
+#
+##############################################################################
 if __name__ == "__main__":
     main()
+    print(ask_for_command())
